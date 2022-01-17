f327586a64d2a6ae55498684dec3d3116e737069
semwai <33517045+semwai@users.noreply.github.com>
2022-01-17 12:17:00 +0300

    add ServiceTestRule

--- app/src/androidTest/java/ru/semwai/android_exam/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/semwai/android_exam/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package ru.semwai.android_exam
 
+import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ServiceTestRule
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.any
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +29,19 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ru.semwai.android_exam", appContext.packageName)
     }
+
+    @get:Rule
+    val serviceRule = ServiceTestRule()
+
+    @Test
+    fun testService() {
+        val serviceIntent = Intent(ApplicationProvider.getApplicationContext<Context>(), MyRandomService::class.java)
+
+        val binder = serviceRule.bindService(serviceIntent)
+
+        val service: MyRandomService = (binder as MyRandomService.LocalBinder).getService()
+
+        assertThat(service.randomNumber, `is`(any(Int::class.java)))
+    }
+
 }
