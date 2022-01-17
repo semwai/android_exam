08eec5d0ea814763f25be9f62204f48dbdd47aad
semwai <33517045+semwai@users.noreply.github.com>
2022-01-17 12:35:35 +0300

    start ActivityTestRule

--- app/src/androidTest/java/ru/semwai/android_exam/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/semwai/android_exam/ExampleInstrumentedTest.kt
@@ -6,6 +6,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -57,6 +58,8 @@ class ExampleInstrumentedTest {
         UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).setOrientationNatural()
 
         Espresso.onView(ViewMatchers.withId(R.id.textView)).perform(ViewActions.click())
+
+        // Espresso.onView(ViewMatchers.withId(R.id.textView)).check(ViewAssertions.matches((ViewMatchers.)))
         Thread.sleep(5000)
     }
 }
